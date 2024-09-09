fba491f997ef11e4e6a813011da36bc1c9cb6689
Snake52996 <33573135+Snake52996@users.noreply.github.com>
2024-09-09 18:32:08 +0800

    add work & use fixed filename

--- convert-image.py
+++ convert-image.py
@@ -1,11 +1,25 @@
 #!/bin/env python
-from pathlib import Path
-from PIL import Image
 from concurrent.futures import ProcessPoolExecutor
-from mimetypes import guess_type
 from json import dump as json_dump
-def process(path: Path):
-  print(path)
+from mimetypes import guess_type
+from pathlib import Path
+
+from PIL import Image
+
+
+def resize(path: Path) -> None:
+  """Resize image."""
+  image = Image.open(path)
+  target_size = (240, 240)
+  if image.size == target_size:
+    return
+  image = image.resize(target_size, resample=Image.Resampling.LANCZOS)
+  image.save(path, "webp")
+  image.close()
+
+
+def convert(path: Path) -> None:
+  """Convert image to WebP and resize."""
   image = Image.open(path)
   image.save(path.with_suffix(".webp"), "webp")
   image.close()
@@ -21,8 +35,11 @@ def process(path: Path):
   }
   with path.with_suffix(".json").open("w", encoding="utf-8") as information_file:
     json_dump(default_information, information_file, indent=2)
+  resize(path)
 
-def main():
+
+def main() -> None:
+  """Convert/resize images."""
   executor = ProcessPoolExecutor()
   for item in Path.cwd().iterdir():
     if not item.is_file():
@@ -31,8 +48,9 @@ def main():
     if guess_result is None or not guess_result.startswith("image/"):
       continue
     if item.suffix == ".webp":
-      continue
-    executor.submit(process, item)
+      executor.submit(resize, item)
+    else:
+      executor.submit(convert, item)
   executor.shutdown(wait=True, cancel_futures=False)
 
 if __name__ == "__main__":
--- crypto.js
+++ crypto.js
@@ -1,12 +1,7 @@
 // ask for password
 const deno = typeof Deno !== 'undefined';
 const ImageHeaderSize = 12;
-const PBKDF2Parameter = {
-  name: "PBKDF2",
-  hash: "SHA-512",
-  iterations: 1000000,
-};
-const AESParameter = { name: "AES-GCM", length: 256 };
+const MetaKey = ["iv", "encrypted_name"];
 async function main() {
   // ask password and prepare it as a base crypto key
   const password = prompt("Please enter the password:");
@@ -14,23 +9,52 @@ async function main() {
   const encoded_password = encoder.encode(password);
   const base_key = await crypto.subtle.importKey("raw", encoded_password, "PBKDF2", false, ["deriveKey"]);
 
+  // create a new meta.json file containing encrypting parameters if one does not exist
+  // this may only happen when running on Deno/encrypting
+  if(deno){
+    try{
+      Deno.statSync("meta.json");
+    } catch (error) {
+      const salt = crypto.getRandomValues(new Uint8Array(16));
+      const PBKDF2Parameter = {
+        name: "PBKDF2",
+        hash: "SHA-512",
+        iterations: 4000000,
+        salt: btoa(String.fromCharCode.apply(null, salt)),
+      };
+      const AESParameter = { name: "AES-GCM", length: 256 };
+      const metadata = {
+        pbkdf2: PBKDF2Parameter,
+        aes: AESParameter,
+      };
+      Deno.writeTextFile("meta.json", JSON.stringify(metadata));
+    }
+  }
+
+  // load metadata from file
+  const meta_string = deno ? Deno.readTextFile("meta.json") : (await fetch(new Request("meta.json"))).text();
+  const metadata = JSON.parse(await meta_string);
+  const PBKDF2Parameter = {
+    ...metadata.pbkdf2,
+    salt: Uint8Array.from(Array.from(atob(metadata.pbkdf2.salt)).map(x => x.charCodeAt(0))),
+  };
+  const AESParameter = metadata.aes;
+
+  // get key
+  const key = await crypto.subtle.deriveKey(
+    PBKDF2Parameter,
+    base_key,
+    AESParameter,
+    false,
+    ["encrypt", "decrypt"]
+  );
+
+
   // encryption mode is used only when running on Deno
   if (deno && Deno.args[0] === "encrypt") {
     // encrypt data files:
     //  1. (webp) images are encrypted in-place with their file header unchanged
     //  2. JSON files are combined together and encrypted into a single file
-
-    // prepare salt for key derivation
-    const salt = new Uint8Array(16);
-    crypto.getRandomValues(salt);
-    // get encryption key
-    const key = await crypto.subtle.deriveKey(
-      { ...PBKDF2Parameter, salt: salt },
-      base_key,
-      AESParameter,
-      false,
-      ["encrypt"]
-    );
 
     // list files and generate list of names to be processed
     let name_list = new Array();
@@ -45,10 +69,18 @@ async function main() {
 
     // function to handle a single name
     async function process(name) {
+      const information = JSON.parse(await Deno.readTextFile(`${name}.json`));
+      if(!Object.keys(information).includes("iv")){
+        const iv = crypto.getRandomValues(new Uint8Array(12));
+        information["iv"] = btoa(String.fromCharCode.apply(null, iv));
+      }
+      if(!Object.keys(information).includes("encrypted_name")){
+        information["encrypted_name"] = crypto.randomUUID();
+      }
       // prepare iv
-      const iv = crypto.getRandomValues(new Uint8Array(12));
+      const iv = Uint8Array.from(Array.from(atob(information.iv)).map(x => x.charCodeAt(0)));
       // prepare new name of the picture: do not leak filenames
-      const new_name = crypto.randomUUID();
+      const new_name = information.encrypted_name;
       async function process_image() {
         const image = await Deno.readFile(`${name}.webp`);
         const encrypted_body = new Uint8Array(await crypto.subtle.encrypt(
@@ -59,13 +91,8 @@ async function main() {
         await Deno.remove(`${name}.webp`);
       }
       async function process_information() {
-        const information = await Deno.readTextFile(`${name}.json`);
         await Deno.remove(`${name}.json`);
-        list[name] = {
-          image_iv: btoa(String.fromCharCode.apply(null, iv)),
-          new_name: new_name,
-          information: information,
-        };
+        list[name] = information;
       }
       return Promise.all([process_image(), process_information()]);
     }
@@ -83,7 +110,6 @@ async function main() {
     ));
     let final_list = {
       iv: btoa(String.fromCharCode.apply(null, iv)),
-      salt: btoa(String.fromCharCode.apply(null, salt)),
       data: btoa(String.fromCharCode.apply(null, encrypted_list)),
     };
     await Deno.writeTextFile("data.json", JSON.stringify(final_list));
@@ -102,17 +128,6 @@ async function main() {
     }
     const final_list = JSON.parse(raw_string);
 
-    // prepare salt
-    const salt = Uint8Array.from(Array.from(atob(final_list["salt"])).map(x => x.charCodeAt(0)));
-    // prepare decryption key
-    const key = await crypto.subtle.deriveKey(
-      { ...PBKDF2Parameter, salt: salt },
-      base_key,
-      AESParameter,
-      false,
-      ["decrypt"]
-    );
-
     // decrypt information
     const iv = Uint8Array.from(Array.from(atob(final_list["iv"])).map(x => x.charCodeAt(0)));
     const encrypted = Uint8Array.from(Array.from(atob(final_list["data"])).map(x => x.charCodeAt(0)));
@@ -127,46 +142,57 @@ async function main() {
 
     async function decrypt_image(image, entry) {
       const iv = Uint8Array.from(
-        Array.from(atob(entry["image_iv"])).map(x => x.charCodeAt(0))
+        Array.from(atob(entry.iv)).map(x => x.charCodeAt(0))
       );
       const body = new Uint8Array(await crypto.subtle.decrypt(
         { name: "AES-GCM", iv }, key, image.slice(ImageHeaderSize)
       ));
-      image.set(body, ImageHeaderSize);
-      return image;
+      let result = new Uint8Array(ImageHeaderSize + body.length);
+      result.set(image.slice(0, ImageHeaderSize));
+      result.set(body, ImageHeaderSize);
+      return result;
     }
 
     async function process_deno(name) {
-      const write_information = Deno.writeTextFile(`${name}.json`, list[name]["information"]);
-      const encrypted_image = await Deno.readFile(`${list[name]["new_name"]}.webp`);
+      const write_information = Deno.writeTextFile(`${name}.json`, JSON.stringify(list[name]));
+      const encrypted_image = await Deno.readFile(`${list[name].encrypted_name}.webp`);
       const image = await decrypt_image(encrypted_image, list[name]);
       await Deno.writeFile(`${name}.webp`, image);
-      await Deno.remove(`${list[name]["new_name"]}.webp`);
+      await Deno.remove(`${list[name].encrypted_name}.webp`);
       await write_information;
     }
 
     async function process_browser(name) {
-      const response = await fetch(new Request(`${list[name]["new_name"]}.webp`));
+      const response = await fetch(new Request(`${list[name].encrypted_name}.webp`));
       const encrypted_image = new Uint8Array(await response.arrayBuffer());
       const image = await decrypt_image(encrypted_image, list[name]);
-      const information = JSON.parse(list[name]["information"]);
+      const information = list[name];
 
       // build node
 
       let work = document.createElement("div");
       work.classList.add("work");
       let cover_container = document.createElement("div");
+      let spacer0 = document.createElement("span");
+      spacer0.classList.add("spacer");
+      let spacer1 = document.createElement("span");
+      spacer1.classList.add("spacer");
       let cover = document.createElement("img");
       const blob = new Blob([image], { type: 'image/webp' });
       const url = URL.createObjectURL(blob);
       cover.src = url;
       let information_container = document.createElement("div");
       for (const key of Object.keys(information)) {
+        if(MetaKey.includes(key)){
+          continue;
+        }
         let node = document.createElement("span");
         node.innerText = `${key}: ${information[key]}`;
         information_container.appendChild(node);
       }
+      cover_container.appendChild(spacer0);
       cover_container.appendChild(cover);
+      cover_container.appendChild(spacer1);
       work.appendChild(cover_container);
       work.appendChild(information_container);
 
