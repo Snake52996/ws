7ad2b689b17906cb5bcbd5ce0365255aa2ccf8e2
Snake52996 <33573135+Snake52996@users.noreply.github.com>
2024-10-11 12:00:56 +0800

    update user script & update work

--- crypto.js
+++ crypto.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
 
 // ws progress tracker
@@ -244,31 +244,113 @@ async function main() {
       const tracked_work = Object.keys(list);
       // prepare CSS style
       GM_addStyle(`
-        div.tracked {
+        .tracked {
           filter: blur(5px);
           transition-duration: 0.4s;
         }
-        div.tracked:hover {
+        .tracked:hover {
           filter: none;
         }
       `);
-      // prepare callback for observer
-      function observer_callback(records, observer) {
-        for (let record of records) {
-          if (record.type !== "childList") {
-            continue;
+      // since we are working with a SPA, we need to detect changes made on href
+      (() => {
+        let last_class = "";
+        const work_observers = {
+          "": null,
+          "work": {
+            "get_work_id": function (href) {
+              if (href.endsWith("/")) {
+                href = href.substring(0, href.length - 1);
+              }
+              return work_id = href.substring(href.lastIndexOf("/") + 1);
+            },
+            "observer": new MutationObserver((records, observer) => {
+              for (let record of records) {
+                const work_id = work_observers.work.get_work_id(record.target.href);
+                if (tracked_work.includes(work_id)) {
+                  document.getElementsByTagName("body")[0].classList.add("tracked");
+                  return;
+                }
+                document.getElementsByTagName("body")[0].classList.remove("tracked");
+              }
+            }),
+            "observe": function (target) {
+              work_observers.work.observer.observe(target, { subtree: true, attributes: true, attributeFilter: ["href"] });
+              for (let node of document.querySelectorAll("a.router-link-active[href]")) {
+                const work_id = work_observers.work.get_work_id(node.href);
+                if (tracked_work.includes(work_id)) {
+                  document.getElementsByTagName("body")[0].classList.add("tracked");
+                  return;
+                }
+              }
+              document.getElementsByTagName("body")[0].classList.remove("tracked");
+            },
+            "disconnect": function () {
+              work_observers.work.observer.disconnect();
+              document.getElementsByTagName("body")[0].classList.remove("tracked");
+            },
+          },
+          "list": {
+            "observer": new MutationObserver((records, observer) => {
+              for (let record of records) {
+                if (record.type !== "childList") {
+                  continue;
+                }
+                for (let node of record.addedNodes) {
+                  if (tracked_work.includes(node.id)) {
+                    node.classList.add("tracked");
+                  }
+                }
+              }
+            }),
+            "observe": function (target) {
+              work_observers.list.observer.observe(target, { childList: true });
+              for (let node of target.childNodes) {
+                if (tracked_work.includes(node.id)) {
+                  node.classList.add("tracked");
+                }
+              }
+            },
+            "disconnect": function () {
+              work_observers.list.observer.disconnect();
+            },
+          },
+        };
+        const work_targets = {
+          "": null,
+          "work": () => (document.querySelector(".q-page-container > div:nth-child(1) > div:nth-child(1)")),
+          "list": () => (document.querySelector("main>div:nth-child(2)")),
+        };
+
+        function get_path_class(path) {
+          if (path.startsWith("/work/")) {
+            return "work";
           }
-          for (let node of record.addedNodes) {
-            if (tracked_work.includes(node.id)) {
-              node.classList.add("tracked");
+          if (path === "/works") {
+            return "list";
+          }
+          return "";
+        }
+        function dispatcher() {
+          const current_class = get_path_class(window.location.pathname);
+          if (current_class !== last_class) {
+            if (work_observers[last_class] !== null) {
+              work_observers[last_class].disconnect();
+            }
+            last_class = current_class;
+            if (work_targets[current_class] !== null) {
+              const target = work_targets[current_class]();
+              if (target === null) {
+                last_class = "";
+                return;
+              }
+              work_observers[current_class].observe(target);
             }
           }
         }
-      }
-      // register observer
-      let observer = new MutationObserver(observer_callback);
-      const target = document.querySelector("main>div:nth-child(2)");
-      observer.observe(target, { childList: true });
+        const dispatcher_observer = new MutationObserver(dispatcher);
+        dispatcher_observer.observe(document, { subtree: true, childList: true });
+      })();
     }
     if (injected) {
       handle_user_script();
